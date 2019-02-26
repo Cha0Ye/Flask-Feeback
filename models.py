d2e2071f5d296e5fe97d067bbd13c5db65cd3fbe
Chao Ye <Chao12.ye@gmail.com>
2019-02-26 11:55:53 -0800

    added User class to model

--- models.py
+++ models.py
@@ -8,3 +8,18 @@ db = SQLAlchemy()
 class User(db.Model):
     '''user class'''
     __tablename__ = 'users'
+
+    username = db.Column(db.String(20), 
+                         primary_key=True)
+
+    password = db.Column(db.Text, 
+                         nullable=False)
+
+    email = db.Column(db.String(50), 
+                      nullable=False)
+
+    first_name = db.Column(db.String(30),
+                           nullable=False)
+
+    last_name = db.Column(db.String(30),
+                           nullable=False)
